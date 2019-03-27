74c4116bbe35804db64d904c1d5271e887029453
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-03-27 21:07:54 +0100

    Showing specific location based on coordinates

--- mapViewApp/ViewController.swift
+++ mapViewApp/ViewController.swift
@@ -13,21 +13,38 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var lattitude = 50.62754
+    var longitude = 30.4431
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        let span = MKCoordinateSpan.init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        
+        let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lattitude, longitude: longitude), span: span)
+        
+        mapView.setRegion(region, animated: true)
+        
     }
 
     @IBAction func standart(_ sender: Any) {
+        
+        mapView.mapType = MKMapType.standard
     }
     
     @IBAction func satelite(_ sender: Any) {
+        
+        mapView.mapType = MKMapType.satellite
     }
     
     @IBAction func hybrid(_ sender: Any) {
+        
+        mapView.mapType = MKMapType.hybrid
     }
     
     @IBAction func locate(_ sender: Any) {
+        
     }
     
     @IBAction func directions(_ sender: Any) {
